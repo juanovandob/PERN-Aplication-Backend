6cbefc37c2dde6ea470ef30b754f17faad87c896
Juanca Ovando <juanovandob@gmail.com>
2023-07-13 15:18:58 -0600

    typeOfPropertyList added
    Resumen de total de propiedades por tipo

--- server/controllers/metrics.controller.js
+++ server/controllers/metrics.controller.js
@@ -1,22 +1,19 @@
-import User from '../db/models/user.js';
-import Property from '../db/models/property.js';
+import User from "../db/models/user.js";
+import Property from "../db/models/property.js";
 //import e from 'express';
 
-
 const getAllMetrics = async (req, res) => {
-    try {
-        res.status(200).json( {message: 'Get all metrics....'});
-    } catch (error) {
-        res.status(500).json({ message: error.message });   
-    }
- };
+  try {
+    res.status(200).json({ message: "Get all metrics...." });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
- //metrics detail by user
+//metrics detail by user
 const getMetricsDetail = async (req, res) => {
-  
   const { id } = req.params;
-  
-  
+
   try {
     const allMetrics = {};
     // Check if user id exists
@@ -26,109 +23,116 @@ const getMetricsDetail = async (req, res) => {
       },
     });
 
-    if(userExists){
+    if (userExists) {
       //getting the count of properties by user
-      const {count, rows} = await Property.findAndCountAll({
+      const { count, rows } = await Property.findAndCountAll({
         where: { creator_id: id },
-        attributes: ["id", "title", "description","photo"],
+        attributes: ["id", "title", "description", "photo", "propertyType"],
         order: [
-          ['id', 'ASC'] // Orden ascendente por la columna 'id'
+          ["id", "ASC"], // Orden ascendente por la columna 'id'
         ],
-        }) //return the count of objects and an ARRAY of objects
+      }); //return the count of objects and an ARRAY of objects
 
-      
       //Title Metrics, count of properties with titles less than 12 characters
       //Description Metrics, count of properties with descriptions less than 100 characters
       //bad and good ones
-      
+
       const allMetrics = {
         totalProperties: {
           count,
         },
         titleQuality: {
-          unhealthy:{
-          count: 0,
-          propertiesList: [],
+          unhealthy: {
+            count: 0,
+            propertiesList: [],
           },
-          healthy:{
-          count: 0,
-          propertiesList: [],
+          healthy: {
+            count: 0,
+            propertiesList: [],
           },
         },
         descriptionQuality: {
-          unhealthy:{
-          count: 0,
-          propertiesList: [],
+          unhealthy: {
+            count: 0,
+            propertiesList: [],
           },
-          healthy:{
-          count: 0,
-          propertiesList: [],
+          healthy: {
+            count: 0,
+            propertiesList: [],
           },
         },
         imageQuality: {
-          unhealthy:{
-          count: 0,
-          propertiesList: [],
+          unhealthy: {
+            count: 0,
+            propertiesList: [],
           },
-          healthy:{
-          count: 0,
-          propertiesList: [],
+          healthy: {
+            count: 0,
+            propertiesList: [],
           },
-        }          
-      };  
-     
+        },
+        propertyType: {
+          propertiesList: [],
+        },
+        //end of allMetrics
+      };
+
+      /*bridgeObject is used to send the propertyType propertiesList to the 
+      allMetrics.propertyType.propertiesList array.*/
+      const bridgeObject = {};
 
       rows.forEach((property) => {
-        if((property.title).length < 12){
+        if (property.title.length < 12) {
           allMetrics.titleQuality.unhealthy.count++;
           allMetrics.titleQuality.unhealthy.propertiesList.push(property);
-        }else{
+        } else {
           allMetrics.titleQuality.healthy.count++;
           allMetrics.titleQuality.healthy.propertiesList.push(property);
-        };
+        }
 
-        if((property.description).length < 100){
+        if (property.description.length < 100) {
           allMetrics.descriptionQuality.unhealthy.count++;
           allMetrics.descriptionQuality.unhealthy.propertiesList.push(property);
-        }else{
+        } else {
           allMetrics.descriptionQuality.healthy.count++;
           allMetrics.descriptionQuality.healthy.propertiesList.push(property);
-        };
+        }
 
-        if(property.image == ""){
+        if (property.image == "") {
           allMetrics.imageQuality.unhealthy.count++;
           allMetrics.imageQuality.unhealthy.propertiesList.push(property);
-        }else{
+        } else {
           allMetrics.imageQuality.healthy.count++;
           allMetrics.imageQuality.healthy.propertiesList.push(property);
-        };
-        console.log("fotos: ", property.photo);
+        }
 
-      //end of forEach  
+        //adding the propertyType to the bridgeObject and adding the counts
+        if (bridgeObject.hasOwnProperty(property.propertyType)) {
+          bridgeObject[property.propertyType] += 1;
+        } else {
+          bridgeObject[property.propertyType] = 1;
+        }
+
+        //end of forEach
       });
-           
-      
+
+      // convert brigdeObject to an array of objects
+      //{type: count, type: count} --> [[type: count], [type: count]] --> [{type: count}, {type: count}]
+      const result = Object.entries(bridgeObject).map(([type, count]) => ({
+        [type]: count,
+      }));
+
+      //sending de bridgeObject to the allMetrics.propertyType.propertiesList array.
+      allMetrics.propertyType.propertiesList = result;
+
       res.status(200).json(allMetrics);
     } else {
       res.status(404).json({ message: "User not found" });
     }
-
   } catch (error) {
     res.status(500).json({ message: error.message });
-    }
- };
-
-//export all the functions
-export {
-    getAllMetrics,
-    getMetricsDetail,
+  }
 };
 
-
-
-
-
-
-
-
-
+//export all the functions
+export { getAllMetrics, getMetricsDetail };
